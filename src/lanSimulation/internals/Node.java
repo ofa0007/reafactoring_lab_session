29b85384826ad1f84d20e15e5b9bf1c713d123e8
Javier Martinez <jmr0082@alu.ubu.es>
2017-05-04 16:53:19 +0200

    Starting to refactor Node by creating workstation and printer

--- src/lanSimulation/internals/Node.java
+++ src/lanSimulation/internals/Node.java
@@ -25,41 +25,44 @@ import java.io.Writer;
 import lanSimulation.Network;
 
 /**
-A <em>Node</em> represents a single Node in a Local Area Network (LAN).
-Several types of Nodes exist.
+ * A <em>Node</em> represents a single Node in a Local Area Network (LAN).
+ * Several types of Nodes exist.
  */
 public class Node {
-	//enumeration constants specifying all legal node types
+	// enumeration constants specifying all legal node types
 	/**
-    A node with type NODE has only basic functionality.
+	 * A node with type NODE has only basic functionality.
 	 */
 	public static final byte NODE = 0;
 	/**
-    A node with type WORKSTATION may initiate requests on the LAN.
+	 * A node with type WORKSTATION may initiate requests on the LAN.
 	 */
 	public static final byte WORKSTATION = 1;
 	/**
-    A node with type PRINTER may accept packages to be printed.
+	 * A node with type PRINTER may accept packages to be printed.
 	 */
 	public static final byte PRINTER = 2;
 
 	/**
-    Holds the type of the Node.
+	 * Holds the type of the Node.
 	 */
 	public byte type_;
 	/**
-    Holds the name of the Node.
+	 * Holds the name of the Node.
 	 */
 	public String name_;
 	/**
-    Holds the next Node in the token ring architecture.
-    @see lanSimulation.internals.Node
+	 * Holds the next Node in the token ring architecture.
+	 * 
+	 * @see lanSimulation.internals.Node
 	 */
 	public Node nextNode_;
 
 	/**
-Construct a <em>Node</em> with given #type and #name.
-<p><strong>Precondition:</strong> (type >= NODE) & (type <= PRINTER);</p>
+	 * Construct a <em>Node</em> with given #type and #name.
+	 * <p>
+	 * <strong>Precondition:</strong> (type >= NODE) & (type <= PRINTER);
+	 * </p>
 	 */
 	public Node(byte type, String name) {
 		assert (type >= NODE) & (type <= PRINTER);
@@ -69,8 +72,11 @@ Construct a <em>Node</em> with given #type and #name.
 	}
 
 	/**
-Construct a <em>Node</em> with given #type and #name, and which is linked to #nextNode.
-<p><strong>Precondition:</strong> (type >= NODE) & (type <= PRINTER);</p>
+	 * Construct a <em>Node</em> with given #type and #name, and which is linked
+	 * to #nextNode.
+	 * <p>
+	 * <strong>Precondition:</strong> (type >= NODE) & (type <= PRINTER);
+	 * </p>
 	 */
 	public Node(byte type, String name, Node nextNode) {
 		assert (type >= NODE) & (type <= PRINTER);
@@ -95,8 +101,11 @@ Construct a <em>Node</em> with given #type and #name, and which is linked to #ne
 	 * <p>
 	 * <strong>Precondition:</strong> isInitialized();
 	 * </p>
-	 * @param network TODO
-	 * @param buf TODO
+	 * 
+	 * @param network
+	 *            TODO
+	 * @param buf
+	 *            TODO
 	 */
 	public void printOn(Network network, StringBuffer buf) {
 		assert network.isInitialized();
@@ -135,12 +144,15 @@ Construct a <em>Node</em> with given #type and #name, and which is linked to #ne
 	 * <p>
 	 * <strong>Precondition:</strong> isInitialized();
 	 * </p>
-	 * @param network TODO
-	 * @param buf TODO
+	 * 
+	 * @param network
+	 *            TODO
+	 * @param buf
+	 *            TODO
 	 */
 	public void printHTMLOn(Network network, StringBuffer buf) {
 		assert network.isInitialized();
-	
+
 		buf.append("<HTML>\n<HEAD>\n<TITLE>LAN Simulation</TITLE>\n</HEAD>\n<BODY>\n<H1>LAN SIMULATION</H1>");
 		Node currentNode = this;
 		buf.append("\n\n<UL>");
@@ -179,12 +191,15 @@ Construct a <em>Node</em> with given #type and #name, and which is linked to #ne
 	 * <p>
 	 * <strong>Precondition:</strong> isInitialized();
 	 * </p>
-	 * @param network TODO
-	 * @param buf TODO
+	 * 
+	 * @param network
+	 *            TODO
+	 * @param buf
+	 *            TODO
 	 */
 	public void printXMLOn(Network network, StringBuffer buf) {
 		assert network.isInitialized();
-	
+
 		Node currentNode = this;
 		buf.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n<network>");
 		do {
---
+++ src/lanSimulation/internals/WorkStation.java
@@ -0,0 +1,10 @@
+package lanSimulation.internals;
+
+public class WorkStation extends Node {
+
+	public WorkStation(byte type, String name) {
+		super(type, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
