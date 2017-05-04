9d029ff951dd8f5cfdd235db6a4679fa462acb48
Javier Martinez <jmr0082@alu.ubu.es>
2017-05-04 16:55:02 +0200

    Fixed constructors for Node Subclasses

--- src/lanSimulation/internals/Printer.java
+++ src/lanSimulation/internals/Printer.java
@@ -2,9 +2,8 @@ package lanSimulation.internals;
 
 public class Printer extends Node {
 
-	public Printer(byte type, String name) {
-		super(type, name);
-		// TODO Auto-generated constructor stub
+	public Printer(String name) {
+		super(PRINTER, name);
 	}
 
 }
--- src/lanSimulation/internals/WorkStation.java
+++ src/lanSimulation/internals/WorkStation.java
@@ -2,9 +2,8 @@ package lanSimulation.internals;
 
 public class WorkStation extends Node {
 
-	public WorkStation(byte type, String name) {
-		super(type, name);
-		// TODO Auto-generated constructor stub
+	public WorkStation(String name) {
+		super(WORKSTATION, name);
 	}
 
 }
